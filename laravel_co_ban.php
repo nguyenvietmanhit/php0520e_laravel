6e160272a0cf2a3a6782217e690f528181944ada
Manh <nvmanh@cmc.com.vn>
2020-10-03 19:41:38 +0700

    Laravel

---
+++ laravel_co_ban.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * laravel_co_ban.php
+ * DEMO ứng dụng CRUD cơ bản sử dụng framework Laravel của PHP
+ * - File .gitignore: GIT bỏ qua các đường dẫn đc
+ *khai báo trong file này, Laravel đã quy định các
+ *đường dẫn trong file này sẽ khác nhau với từng
+ *local
+ */
